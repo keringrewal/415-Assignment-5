79d126bb29d78f0be455fe16e79b00ae29f7d857
Janie Briglio <janiebriglio@dhcp-wifi-8021x-155-41-83-179.bu.edu>
2018-03-19 14:10:33 -0400

    buoy data

---
+++ bouy.R
@@ -0,0 +1,94 @@
+yr1985<- filter(X1985, `hh`==12 )
+yr1985 <- yr1985 %>% select(`MM`, `DD`, `ATMP`, `WTMP`) 
+yr1985 <- yr1985[-c(1:19),]
+
+yr1986<- filter(X1986, `hh`==12 )
+yr1986 <- yr1986 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr1987<- filter(X1987, `hh`==12 )
+yr1987 <- yr1987 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr1988<- filter(X1988, `hh`==12 )
+yr1988 <- yr1988 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr1990<- filter(X1990, `hh`==12 )
+yr1990 <- yr1990 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+X1991 <- dataset
+yr1991<- filter(X1991, `hh`==12 )
+yr1991 <- yr1991 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr1992<- filter(X1992, `hh`==12 )
+yr1992 <- yr1992 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr1993<- filter(X1993, `hh`==12 )
+yr1993 <- yr1993 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr1994<- filter(X1994, `hh`==12 )
+yr1994 <- yr1994 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr1995<- filter(X1995, `hh`==12 )
+yr1995 <- yr1995 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr1996<- filter(X1996, `hh`==12 )
+yr1996 <- yr1996 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr1997<- filter(X1997, `hh`==12 )
+yr1997 <- yr1997 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr1998<- filter(X1998, `hh`==12 )
+yr1998 <- yr1998 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr1999<- filter(X1999, `hh`==12 )
+yr1999 <- yr1999 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr2000<- filter(X2000, `hh`==12 )
+yr2000 <- yr2000 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr2001<- filter(X2001, `hh`==12 )
+yr2001 <- yr2001 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr2002<- filter(X2002, `hh`==12 )
+yr2002 <- yr2002 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr2003<- filter(X2003, `hh`==12 )
+yr2003 <- yr2003 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr2004<- filter(X2004, `hh`==12 )
+yr2004 <- yr2004 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr2005<- filter(X2005, `hh`==12 )
+yr2005 <- yr2005 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr2006<- filter(X2006, `hh`==12 )
+yr2006 <- yr2006 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr2007<- filter(X2007, `hh`==12 )
+yr2007 <- yr2007 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr2008<- filter(X2008, `hh`==12 )
+yr2008 <- yr2008 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr2009<- filter(X2009, `hh`==12 )
+yr2009 <- yr2009 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr2010<- filter(X2010, `hh`==12 )
+yr2010 <- yr2010 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr2011<- filter(X2011, `hh`==12 )
+yr2011 <- yr2011 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr2012<- filter(X2012, `hh`==12 )
+yr2012 <- yr2012 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr2014<- filter(X2014, `hh`==12 )
+yr2014 <- yr2014 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr2015<- filter(X2015, `hh`==12 )
+yr2015 <- yr2015 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr2016<- filter(X2016, `hh`==12 )
+yr2016 <- yr2016 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
+
+yr2017<- filter(X2010, `hh`==12 )
+yr2017 <- yr2017 %>% select(`MM`, `DD`, `ATMP`, `WTMP`)
